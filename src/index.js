7a9289435e7ede5a9465d00571bfa312ef1a764c
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-12-13 23:25:07 +0300

    console display

--- src/index.js
+++ src/index.js
@@ -18,27 +18,22 @@ const renderShowing = (film) => {
 
   document.getElementById(`poster`).src = film.poster;
   document.getElementById(`title`).innerHTML = film.title;
+  document.getElementById(`film-info`).innerHTML = film.description;
   document.getElementById(`runtime`).innerHTML = film.runtime;
   document.getElementById(`showtime`).innerHTML = film.showtime;
+  document.getElementById(`capacity`).innerHTML = film.capacity;
+  document.getElementById(`tickets_sold`).innerHTML = film.tickets_sold;
   document.getElementById(`available-tickets`).innerHTML =
     film.availabletickets;
-  console.log(film);
-};
-
-const renderMoreContent = (film) => {
-  const buyTicketButton = document.getElementById(`buy-ticket`);
-  buyTicketButton.addEventListener(`click`, () => {
-    const availableTickets = parseInt(
-      document.getElementById(`available-tickets`).innerHTML
-    );
-
-    if (availableTickets > 0) {
-      const newAvailableTickets = capacity - 1;
-
-      document.getElementById(`available-tickets`).innerHTML =
-        newAvailableTickets;
-    } else {
-      alert("sorry,tickets sold out");
-    }
+  const available_tickets = film.capacity - film.tickets_sold;
+  console.log(available_tickets);
+  // film.availabletickets.appendChild(available_tickets);
+  const button = document.getElementById(`buy-ticket`);
+  button.addEventListener(`click`, () => {
+    buyTicketButton(films);
   });
+};
+const buyTicketButton = () => {
+  const available_tickets = films.capacity - films.tickets_sold;
+  return available_tickets;
 };

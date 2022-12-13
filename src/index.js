ed1fa75b19394288a3b457ca6131ee865c5f2f7e
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-12-14 02:17:17 +0300

    buyticketbutton

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
+//making a request to GET data
 fetch("http://localhost:3000/films")
   .then((resp) => resp.json())
   .then((films) => renderFilms(films));
 
+// const buyTickets = document.querySelector("#buy-ticket");
+// buyTickets.addEventListener(`submit`, handleBuyTickets);
+
 const renderFilms = (films) => {
-  console.log(films);
+  // console.log(films);
   const filmsList = document.getElementById(`films`);
-  films.forEach((film, i) => {
+  films.forEach((film) => {
     const li = document.createElement("li");
     li.innerText = film.title;
     filmsList.append(li);
     li.addEventListener(`click`, () => renderShowing(film));
   });
 };
-
+//displaying film details on the page
 const renderShowing = (film) => {
-  console.log(film);
-
+  // console.log(film);
   document.getElementById(`poster`).src = film.poster;
   document.getElementById(`title`).innerHTML = film.title;
   document.getElementById(`film-info`).innerHTML = film.description;
@@ -23,17 +26,29 @@ const renderShowing = (film) => {
   document.getElementById(`showtime`).innerHTML = film.showtime;
   document.getElementById(`capacity`).innerHTML = film.capacity;
   document.getElementById(`tickets_sold`).innerHTML = film.tickets_sold;
-  document.getElementById(`available-tickets`).innerHTML =
-    film.availabletickets;
   const available_tickets = film.capacity - film.tickets_sold;
-  console.log(available_tickets);
-  // film.availabletickets.appendChild(available_tickets);
-  const button = document.getElementById(`buy-ticket`);
-  button.addEventListener(`click`, () => {
-    buyTicketButton(films);
-  });
+  // console.log(available_tickets);
+  document.getElementById(
+    "available-tickets"
+  ).innerHTML = `${available_tickets} available tickets`;
 };
-const buyTicketButton = () => {
-  const available_tickets = films.capacity - films.tickets_sold;
-  return available_tickets;
-};
+
+const button = document.getElementById(`buy-ticket`);
+button.addEventListener(`click`, () => {
+  const available_Tickets = parseInt(
+    document.getElementById(`available-tickets`).innerHTML
+  );
+  if (available_Tickets > 0) {
+    const availableTickets = available_Tickets -=1;
+    document.getElementById(`available-tickets`).innerHTML = availableTickets;
+  } else {
+    alert("sorry,tickets  are sold out.");
+  }
+});
+
+// function handleBuyTickets(films) {
+//   e.preventDefault();
+//   let buyTicket = (document.getElementById(
+//     "available-tickets"
+//   ).innerHTML = `${available_tickets} available tickets`);
+// }
